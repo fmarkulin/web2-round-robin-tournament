a61652a9543af9e19f9dfb4a491da56b5bf0894c
Fran Markulin <fran@markulin.com>
2023-10-15 22:22:27 +0200

    add table skeleton for loading

--- app/tournaments/[slug]/loading.tsx
+++ app/tournaments/[slug]/loading.tsx
@@ -14,7 +14,7 @@ export default function TournamentLoading() {
     <div className="max-w-7xl mx-auto">
       <Skeleton className="w-4/5 sm:w-[500px] h-[45px] mb-1" />
       <Skeleton className="w-[120px] h-[35px] mb-6" />
-      <div className="flex">
+      <div className="flex flex-col justify-between gap-16 md:flex-row md:gap-4">
         <div className="flex flex-col gap-10">
           {Array.from({ length: 3 }, (_, i) => i + 1).map((_, index) => (
             <div key={index}>
@@ -55,6 +55,29 @@ export default function TournamentLoading() {
             </div>
           ))}
         </div>
+        <Table className="border-2 w-auto mx-auto">
+          <TableCaption>
+            <h5 className="text-lg font-medium mb-2">Current lineup</h5>
+          </TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Player</TableHead>
+              <TableHead>Points</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 4 }, (_, i) => i + 1).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <Skeleton className="w-[115px] h-[20px]" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="w-[20px] h-[20px]" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
